4ee817070ccdbe2d42f2dc6a21a17f91aee91e91
Wanako <84190429+Wanako0609@users.noreply.github.com>
2023-01-19 23:48:24 +0100

    ForetsAxe Create

---
+++ src/main/java/net/helshim/helshimmod/ServerProxyHelshimMod.java
@@ -0,0 +1,24 @@
+package net.helshim.helshimmod;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+public class ServerProxyHelshimMod implements IProxyHelshimMod {
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
