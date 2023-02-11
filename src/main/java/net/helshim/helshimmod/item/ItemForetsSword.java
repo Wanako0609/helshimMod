fab1bf9f7b8152148133659d7e938453f139e10d
Wanako <84190429+Wanako0609@users.noreply.github.com>
2023-02-12 00:14:00 +0100

    Ajout de la Forets Sword

---
+++ src/main/java/net/helshim/helshimmod/item/ItemForetsSword.java
@@ -0,0 +1,63 @@
+
+package net.helshim.helshimmod.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+
+import net.helshim.helshimmod.ElementsHelshimMod;
+
+import java.util.Set;
+import java.util.HashMap;
+
+import com.google.common.collect.Multimap;
+
+@ElementsHelshimMod.ModElement.Tag
+public class ItemForetsSword extends ElementsHelshimMod.ModElement {
+	@GameRegistry.ObjectHolder("helshimmod:foretssword")
+	public static final Item block = null;
+	public ItemForetsSword(ElementsHelshimMod instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemSword(EnumHelper.addToolMaterial("FORETSSWORD", 1, 100, 4f, 2f, 2)) {
+			@Override
+			public Multimap<String, AttributeModifier> getItemAttributeModifiers(EntityEquipmentSlot slot) {
+				Multimap<String, AttributeModifier> multimap = super.getItemAttributeModifiers(slot);
+				if (slot == EntityEquipmentSlot.MAINHAND) {
+					multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(),
+							new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double) this.getAttackDamage(), 0));
+					multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(),
+							new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", -2, 0));
+				}
+				return multimap;
+			}
+
+			public Set<String> getToolClasses(ItemStack stack) {
+				HashMap<String, Integer> ret = new HashMap<String, Integer>();
+				ret.put("sword", 1);
+				return ret.keySet();
+			}
+		}.setUnlocalizedName("foretssword").setRegistryName("foretssword").setCreativeTab(CreativeTabs.COMBAT));
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("helshimmod:foretssword", "inventory"));
+	}
+}
